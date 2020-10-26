f7d8323a88bb7b57684520053ec3c98e23a42da2
OfficialNoFocus <mickvanderwoude@gmail.com>
2020-10-26 23:03:51 +0100

    if statement fix for bad url(id wrong)

--- admin-kanidaten/select.php
+++ admin-kanidaten/select.php
@@ -3,7 +3,7 @@
     define('REMOTE_URL','http://school.local');
     require_once($_SERVER['DOCUMENT_ROOT']."/API/Database.php");
     require_once($_SERVER['DOCUMENT_ROOT']."/API/Settings.php");
-    // require($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php');
+    require($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php');
     // dd( intval($_GET['id']));
     // dd((new API\Database(API\Settings::getDatabaseCredentials()))->getUser(1));
     // dd((new API\Database(API\Settings::getDatabaseCredentials()))->getUserPhotos(1));
@@ -11,13 +11,13 @@
     if($id > 0) {
         $grapher = (new API\Database(API\Settings::getDatabaseCredentials()))->getUser($id);
 
-        $media = (new API\Database(API\Settings::getDatabaseCredentials()))->getUserVideo($id);
-        if (empty($media)) {
-            $media = (new API\Database(API\Settings::getDatabaseCredentials()))->getUserPhotos($id);
+        $media = (new API\Database(API\Settings::getDatabaseCredentials()))->getUserPhotos($id);
+        if(!$media){
+            $media = (new API\Database(API\Settings::getDatabaseCredentials()))->getUserVideo($id);
         }
-        else {
-            //return header("HTTP/1.0 404 Not Found");
-        }
+        if(!$media){
+            return header("HTTP/1.0 404 Not Found");
+        } 
     }
     else {
         return header("HTTP/1.0 404 Not Found");
@@ -32,6 +32,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <!-- can be replaced with bootstrap cdn -->
     <link href="<?php echo(REMOTE_URL) ?>/css/app.css" rel="stylesheet">
 </head>
 <body>
